d30b952f18e0da39e3638a9de9d1b0f6e7fdc604
Emilio Barrera Sepúlveda <barrera_emilio@yahoo.es>
2024-03-13 18:17:53 +0000

    Add Class HttpResponseWrapper<T>

---
+++ Orders/Orders.Frontend/Repositories/HttpResponseWrapper.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+
+namespace Orders.Frontend.Repositories
+{
+    public class HttpResponseWrapper<T>
+    {
+        public HttpResponseWrapper(T? response, bool error, HttpResponseMessage httpResponseMessage)
+        {
+            Response = response;
+            Error = error;
+            HttpResponseMessage = httpResponseMessage;
+        }
+
+        public T? Response { get; }
+        public bool Error { get; }
+        public HttpResponseMessage HttpResponseMessage { get; }
+
+        public async Task<string> GetErrorMessageAsync()
+        {
+            if (!Error)
+            {
+                return null;
+            }
+
+            var statusCode = HttpResponseMessage.StatusCode;
+
+            if (statusCode == HttpStatusCode.NotFound)
+            {
+                return "Recuso no Encontrado!";
+            }
+
+            if (statusCode == HttpStatusCode.BadRequest)
+            {
+                return await HttpResponseMessage.Content.ReadAsStringAsync();
+            }
+
+            if (statusCode == HttpStatusCode.Unauthorized)
+            {
+                return "Tienes que esta Logiado para ejecutar esta Operacion.";
+            }
+
+            return "Ha ucurrido un Error inesperado!";
+        }
+    }
+}
