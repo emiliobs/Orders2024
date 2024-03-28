e5b621921bc49c30350727148d3b61d72205d1db
Emilio Barrera Sepúlveda <barrera_emilio@yahoo.es>
2024-03-28 18:58:58 +0000

    Cretete a neww country part 1

---
+++ Orders/Orders.Frontend/Pages/Countries/CountryForm.razor.cs
@@ -0,0 +1,27 @@
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Orders.Shared.Entities;
+
+namespace Orders.Frontend.Pages.Countries
+{
+    public partial class CountryForm
+    {
+        private EditContext editContext = null!;
+
+        [EditorRequired, Parameter]
+        public Country Country { get; set; } = null!;
+
+        [EditorRequired, Parameter]
+        public EventCallback OnValidSubmit { get; set; }
+
+        [EditorRequired, Parameter]
+        public EventCallback ReturnValue { get; set; }
+
+        [Inject]
+        public SweetAlertService SweetAlertService { get; set; } = null!;
+
+        public bool FormPostPostedSuccessFully { get; set; }
+
+    }
+}
