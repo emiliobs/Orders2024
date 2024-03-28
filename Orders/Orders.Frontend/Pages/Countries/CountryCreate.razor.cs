f9509afe7b3be538470c4a3435c3d4e9463c8d18
Emilio Barrera Sepúlveda <barrera_emilio@yahoo.es>
2024-03-28 19:38:01 +0000

    countryForm and create country  parte 1

---
+++ Orders/Orders.Frontend/Pages/Countries/CountryCreate.razor.cs
@@ -0,0 +1,64 @@
+﻿// Importing the SweetAlert2 library from the CurrieTechnologies.Razor namespace
+using CurrieTechnologies.Razor.SweetAlert2;
+
+// Importing the ComponentBase class from the Microsoft.AspNetCore.Components namespace
+using Microsoft.AspNetCore.Components;
+
+// Importing the IRepository and Country classes from respective namespaces
+using Orders.Frontend.Repositories;
+using Orders.Shared.Entities;
+
+// Declaring a namespace for the CountryCreate class within the Orders.Frontend.Pages.Countries namespace
+namespace Orders.Frontend.Pages.Countries
+{
+    // Declaring the partial class CountryCreate
+    public partial class CountryCreate
+    {
+        // Declaring a private instance variable 'country' of type Country and initializing it with a new instance of Country
+        private Country country = new Country();
+
+        // Declaring a private instance variable 'countryForm' of type CountryForm
+        private CountryForm countryForm;
+
+        // Declaring a public property 'Repository' of type IRepository and injecting it
+        [Inject]
+        public IRepository Repository { get; set; } = null!;
+
+        // Declaring a public property 'SweetAlertService' of type SweetAlertService and injecting it
+        [Inject]
+        public SweetAlertService SweetAlertService { get; set; } = null!;
+
+        // Declaring a public property 'NavigationManager' of type NavigationManager and injecting it
+        [Inject]
+        public NavigationManager NavigationManager { get; set; } = null!;
+
+        // Declaring a private asynchronous method 'CreateAsync'
+        private async Task CreateAsync()
+        {
+            // Sending a POST request to the '/api/countries' endpoint with the 'country' object
+            var responseHttp = await Repository.PostAsync("/api/countries", country);
+            // Checking if there's an error in the response
+            if (responseHttp.Error)
+            {
+                // Retrieving the error message
+                var message = await responseHttp.GetErrorMessageAsync();
+                // Displaying a SweetAlert modal with the error message
+                await SweetAlertService.FireAsync("Error", message);
+                // Exiting the method
+                return;
+            }
+
+            // Invoking the 'Return' method
+            Return();
+        }
+
+        // Declaring a private method 'Return'
+        private void Return()
+        {
+            // Setting the 'FormPostPostedSuccessFully' property of 'countryForm' to true
+            countryForm.FormPostPostedSuccessFully = true;
+            // Navigating to the '/countries' page
+            NavigationManager.NavigateTo("/countries");
+        }
+    }
+}
--- Orders/Orders.Frontend/Pages/Countries/CountryForm.razor.cs
+++ Orders/Orders.Frontend/Pages/Countries/CountryForm.razor.cs
@@ -1,6 +1,7 @@
 ﻿using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Routing;
 using Orders.Shared.Entities;
 
 namespace Orders.Frontend.Pages.Countries
@@ -16,12 +17,40 @@ namespace Orders.Frontend.Pages.Countries
         public EventCallback OnValidSubmit { get; set; }
 
         [EditorRequired, Parameter]
-        public EventCallback ReturnValue { get; set; }
+        public EventCallback ReturnAction { get; set; }
 
         [Inject]
         public SweetAlertService SweetAlertService { get; set; } = null!;
 
         public bool FormPostPostedSuccessFully { get; set; }
+
+        private async Task OnBeforeInternalNavigation(LocationChangingContext context)
+        {
+            var formWasEdited = editContext.IsModified();
+            if (!formWasEdited || FormPostPostedSuccessFully)
+            {
+                return;
+            }
+
+            var result = await SweetAlertService.FireAsync(new SweetAlertOptions
+            {
+                Title = "Confirmación",
+                Text = "Deseas abandonar la página y perder los cambios?",
+                Icon = SweetAlertIcon.Question,
+                ShowCancelButton = true,
+
+            });
+
+            var confirm = !string.IsNullOrEmpty(result.Value);
+
+            if (confirm)
+            {
+                return;
+            }
+
+            context.PreventNavigation();
+
+        }
 
     }
 }
